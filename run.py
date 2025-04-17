a04a54eb96c54dfd013795adc9ad125a051bbfd1
Ping Jkp Jiang <jiang_kangping@163.com>
2025-04-17 18:48:21 +0800

    some

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import matplotlib
 import numpy as np
 import os
 import torch
+import gradio as gr
 
 from depth_anything_v2.dpt import DepthAnythingV2
 
@@ -24,8 +25,8 @@ def inspect_array(arr):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Depth Anything V2')
     
-    parser.add_argument('--img-path', type=str, default='assets/examples/demo01.jpg')
-    parser.add_argument('--input-size', type=int, default=518)
+    parser.add_argument('--img-path', type=str, default='')
+    parser.add_argument('--input-size', type=int, default=1024)
     parser.add_argument('--outdir', type=str, default='./vis_depth')
     
     parser.add_argument('--encoder', type=str, default='vitl', choices=['vits', 'vitb', 'vitl', 'vitg'])
@@ -47,41 +48,64 @@ if __name__ == '__main__':
     depth_anything = DepthAnythingV2(**model_configs[args.encoder])
     depth_anything.load_state_dict(torch.load(f'checkpoints/depth_anything_v2_{args.encoder}.pth', map_location='cpu'))
     depth_anything = depth_anything.to(DEVICE).eval()
-    
-    if os.path.isfile(args.img_path):
-        if args.img_path.endswith('txt'):
-            with open(args.img_path, 'r') as f:
-                filenames = f.read().splitlines()
-        else:
-            filenames = [args.img_path]
-    else:
-        filenames = glob.glob(os.path.join(args.img_path, '**/*'), recursive=True)
-    
-    os.makedirs(args.outdir, exist_ok=True)
-    
-    cmap = matplotlib.colormaps.get_cmap('Spectral_r')
-    
-    for k, filename in enumerate(filenames):
-        print(f'Progress {k+1}/{len(filenames)}: {filename}')
-        
-        raw_image = cv2.imread(filename)
+
+    def infer_image(image):
+        image = image.convert('RGB')
+        raw_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
         
         depth = depth_anything.infer_image(raw_image, args.input_size)
-        inspect_array(depth)
         
-        depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-        inspect_array(depth)
+        depth = (depth - depth.min()) / (depth.max() - depth.min() + 1e-6) * 255.0
         depth = depth.astype(np.uint8)
-        
-        if args.grayscale:
-            depth = np.repeat(depth[..., np.newaxis], 3, axis=-1)
+        return depth
+
+    iface = gr.Interface(
+        fn=infer_image,
+        inputs=gr.Image(type="pil"),
+        outputs=gr.Image(type="numpy"),
+        title="Depth Anything V2",
+        description="Upload an image to generate its depth map using Depth Anything V2."
+        )
+
+    iface.launch(server_port=7860)
+    
+    if args.img_path:
+        if os.path.isfile(args.img_path):
+            if args.img_path.endswith('txt'):
+                with open(args.img_path, 'r') as f:
+                    filenames = f.read().splitlines()
+            else:
+                filenames = [args.img_path]
         else:
-            depth = (cmap(depth)[:, :, :3] * 255)[:, :, ::-1].astype(np.uint8)
+            filenames = glob.glob(os.path.join(args.img_path, '**/*'), recursive=True)
         
-        if args.pred_only:
-            cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '.png'), depth)
-        else:
-            split_region = np.ones((raw_image.shape[0], 50, 3), dtype=np.uint8) * 255
-            combined_result = cv2.hconcat([raw_image, split_region, depth])
+        os.makedirs(args.outdir, exist_ok=True)
+        
+        cmap = matplotlib.colormaps.get_cmap('Spectral_r')
+        
+        for k, filename in enumerate(filenames):
+            print(f'Progress {k+1}/{len(filenames)}: {filename}')
             
-            cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '.png'), combined_result)
+            raw_image = cv2.imread(filename)
+            
+            depth = depth_anything.infer_image(raw_image, args.input_size)
+            inspect_array(depth)
+            
+            depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+            inspect_array(depth)
+            depth = depth.astype(np.uint8)
+            
+            if args.grayscale:
+                depth = np.repeat(depth[..., np.newaxis], 3, axis=-1)
+            else:
+                depth = (cmap(depth)[:, :, :3] * 255)[:, :, ::-1].astype(np.uint8)
+            
+            if args.pred_only:
+                cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '.png'), depth)
+            else:
+                split_region = np.ones((raw_image.shape[0], 50, 3), dtype=np.uint8) * 255
+                combined_result = cv2.hconcat([raw_image, split_region, depth])
+                
+                cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '.png'), combined_result)
+    
+    
